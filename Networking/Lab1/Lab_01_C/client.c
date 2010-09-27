5309543b059c0364233c0c78b81f23bc6cc8549b
Cody Thompson <Kir.Serenity+github@gmail.com>
2010-09-27 14:54:19 -0400

    Added the second and third labs

---
+++ Networking/Lab1/Lab_01_C/client.c
@@ -0,0 +1,127 @@
+/*
+ * =====================================================================================
+ *
+ *                 Student Name: Cody Thompson
+ *             Algonquin E-Mail: thom0731
+ *               Student Number: 040 584 188
+ *                Course Number: CST8165
+ *                  Lab Section: 010
+ *               Professor Name: David Watson
+ *  Assignment Name/Number/Date: Lab 1 due September 27th, 2010
+ *
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define BUF_SIZ 16
+
+#define ADDRESS 1
+#define PORT 2
+#define FILENAME 3
+
+/* Connects to the server and sends the file via IPv4
+ * @author Cody Thompson
+ * @param argc Number of arguments sent
+ * @param argv Pointer to the array of arguments
+ */
+int main(int argc, char* argv[])
+{
+    int sockfd; /* Socket File Descriptor */
+    int bytesSent = 0; /* Bytes Sent */
+    int status; /* Placeholder for status return */
+    int length; /* Holds the length of the data */
+    struct addrinfo hints, *res; /* Network Structures */
+    char buf[BUF_SIZ]; /* Buffer */
+    char* address;
+    char* port;
+    char* file;
+
+    /* Check the parameters */
+    if(argc < 4)
+    {
+        fprintf(stderr, "No port provided!\n");
+        fprintf(stderr, "Proper usage:\n");
+        fprintf(stderr, "   ./client address portNumber file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Define the parameters */
+    address = argv[ADDRESS];
+    port = argv[PORT];
+    file = argv[FILENAME];
+
+    /* Setup the structures */
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET6;
+    hints.ai_socktype = SOCK_STREAM;
+    if((status = getaddrinfo(address, "1024", &hints, &res) != 0))
+    {
+        fprintf(stderr, "getaddrinfo failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Create the socket */
+    sockfd = socket(res->ai_family, res->ai_socktype,
+            res->ai_protocol);
+
+    /* Connect to the server */
+    if((status = connect(sockfd, res->ai_addr, 
+        res->ai_addrlen) != 0))
+    {
+        fprintf(stderr, "Could not connect!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Send the file */
+    bytesSent = sendFile(sockfd, file);
+
+    /* Close the socket */
+    close(sockfd);
+
+    /* Garbage clean-up */
+    freeaddrinfo(res);
+
+    /* Show the number of bytes sent */
+    printf("Number of bytes sent: %d\n", bytesSent);
+
+    return 0;
+}
+int sendFile(int sockfd, char* file)
+{
+    FILE* f;
+    int totalSent;
+    int bytesSent;
+    int fileRead;
+    char buf[BUF_SIZ];
+
+    /* Open the file */
+    if((f = fopen(file, "rb")) == NULL)
+    {
+        fprintf(stderr, "Could not open file %s\n", file);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Read the file */
+    totalSent = 0;
+    while((fileRead = fread(buf, 1, BUF_SIZ - 1, f)) != 0)
+    {
+        bytesSent = 0;
+
+        /* Send the file */
+        while(bytesSent < fileRead)
+            bytesSent += send(sockfd, buf + bytesSent, fileRead, 0);
+
+        /* Add to the total */
+        totalSent += bytesSent;
+    }
+
+    /* Return the number of bytes */
+    return totalSent;
+}
---
+++ Networking/Lab1/Lab_01_C/server2.c
@@ -0,0 +1,142 @@
+/*
+ * =====================================================================================
+ *
+ *                 Student Name: Cody Thompson
+ *             Algonquin E-Mail: thom0731
+ *               Student Number: 040 584 188
+ *                Course Number: CST8165
+ *                  Lab Section: 010
+ *               Professor Name: David Watson
+ *  Assignment Name/Number/Date: Lab 1 due September 27th, 2010
+ *
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUF_SIZ 16
+
+#define FILENAME "Lab_01_a"
+
+void dostuff(int, char*); /* function prototype */
+
+/* Uses perror and exists the program
+ * @param msg Message for the error
+ */
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+/* Hosts a server by configuring a socket and 
+ * listenning on a port
+ * @author Cody Thompson
+ * @param argc Number of arguments sent
+ * @param argv Pointer to the array of arguments
+ */
+int main(int argc, char *argv[])
+{
+    char* dir;
+    int sockfd, newsockfd, portno, clilen, pid;
+    struct sockaddr_in6 serv_addr, cli_addr;
+
+    if (argc < 3) {
+        fprintf(stderr,"Insufficient parameters!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Assign the parameters */
+    portno = atoi(argv[1]);
+    dir = argv[2];
+
+    /* Open a Socket */
+    sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    /* Clear the memory */
+    memset(&serv_addr, 0, sizeof(serv_addr));
+
+    /* Set the struct */
+    serv_addr.sin6_family = AF_INET6;
+    /* serv_addr.sin_addr.s_addr = INADDR_ANY; */
+    serv_addr.sin6_port = htons(portno);
+
+    /* Bind to the Port and Listen on it */
+    if (bind(sockfd, (struct sockaddr *) &serv_addr,
+            sizeof(serv_addr)) < 0) 
+        error("ERROR on binding");
+    listen(sockfd,5);
+
+    /* Incoming Client Request */
+    clilen = sizeof(cli_addr);
+
+    /* Receive the text */
+    while (2) 
+    {
+        newsockfd = accept(sockfd, 
+            (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) 
+            error("ERROR on accept");
+        pid = fork();
+        if (pid < 0)
+            error("ERROR on fork");
+        if (pid == 0)  {
+            close(sockfd);
+            dostuff(newsockfd, dir);
+            exit(0);
+        }
+        else close(newsockfd);
+    } /* end of while */
+    return 0; /* we never get here */
+}
+
+/* Reads from the socket and writes it to a file
+ * @author Cody Thompson
+ * @param sock The file descriptor of the socket
+ * @param dir The directory to write the file in
+ */
+void dostuff (int sock, char* dir)
+{
+    FILE* f;
+    int readBytes;
+    char buf[BUF_SIZ];
+    struct stat st;
+
+    /* Empty the Memory */
+    memset(buf, 0, sizeof(buf));
+
+    /* Check for directory */
+    if(stat(dir, &st) == -1)
+    {
+        /* Create the directory */
+        mkdir(dir, 0777);
+    }
+
+    /* Change to the directory */
+    chdir(dir);
+
+    /* Open the file */
+    f = fopen(FILENAME, "wb");
+
+    /* Read in a number of characters from the socket
+     * and write it to the file
+     */
+    while((readBytes = read(sock,buf, BUF_SIZ - 1)) > 0)
+    {
+        fwrite(buf, 1, readBytes, f);
+    }
+
+    /* Print to the Console */
+    printf("Wrote file to location %s/%s", dir, FILENAME);
+
+    /* Close the file */
+    fclose(f);
+}
