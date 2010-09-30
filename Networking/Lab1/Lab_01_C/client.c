38af048ad46538cc4a77aef93a4b04f584c09064
Cody Thompson <Kir.Serenity+github@gmail.com>
2010-09-30 12:44:39 -0400

    Added submissions for the secondary parts of the Lab

--- Networking/Lab1/Lab_01_C/client.c
+++ Networking/Lab1/Lab_01_C/client.c
@@ -61,7 +61,7 @@ int main(int argc, char* argv[])
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET6;
     hints.ai_socktype = SOCK_STREAM;
-    if((status = getaddrinfo(address, "1024", &hints, &res) != 0))
+    if((status = getaddrinfo(address, port, &hints, &res) != 0))
     {
         fprintf(stderr, "getaddrinfo failed!\n");
         exit(EXIT_FAILURE);
