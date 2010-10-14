14a0b29929dbfe9b0703da001e1c255f14a7f209
Aretnorp <Kir.Serenity+github@gmail.com>
2010-10-14 13:59:01 -0400

    Updated Assignment 2 to complete

--- Graphics/Assignment2/Thompson_02_8263.c
+++ Graphics/Assignment2/Thompson_02_8263.c
@@ -1,7 +1,7 @@
 /*
  * =====================================================================================
  *
- *       Filename:  Thompson_01_8263.c
+ *       Filename:  Thompson_02_8263.c
  *
  *    Description:  Provides an interface to create Star Polygons
  *
@@ -22,6 +22,7 @@
  *  Include the headers
  *-----------------------------------------------------------------------------*/
 #include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
 #include <math.h>
 #include <stdio.h>
@@ -31,17 +32,27 @@
 #define TRUE 1
 #define FALSE 0
 
-#define DEFAULT_FONT GLUT_BITMAP_8_BY_13
-#define BUF_SIZ 256
+#define NUM_LISTS 2
 
 /*-----------------------------------------------------------------------------
  *  Function Definitions
  *-----------------------------------------------------------------------------*/
 void Draw( void );
-void DrawLine( float , float , float, float );
-void KeyboardFunc( unsigned char , int , int );
-void DrawText( float x, float y, void* font, char* buf);
+void Reshape( int, int );
+void Keyboard( unsigned char , int , int );
 void ClearMemory( void );
+void InitShapes( void );
+
+/*-----------------------------------------------------------------------------
+ *  Variable Definitions
+ *-----------------------------------------------------------------------------*/
+int theta = 0;
+int beta = 0;
+int radius = 250;
+
+GLuint tLists;
+GLenum mode = GL_FILL;
+char letter = 't';
 
 /*-----------------------------------------------------------------------------
  *  Main Function
@@ -49,17 +60,14 @@ void ClearMemory( void );
  *-----------------------------------------------------------------------------*/
 int main( int argc, char *argv[] )
 {
-    /* Seed the random number generator */
-    srand(time(0));
-
     /* Initialize the drawing environment */
     glutInit( &argc, argv );
 
     /* Initialize the display mode */
-    glutInitDisplayMode ( GLUT_DOUBLE );
+    glutInitDisplayMode ( GLUT_DOUBLE | GLUT_DEPTH );
 
     /* Create an application window of a certain size */
-    glutInitWindowSize( 850, 450 );
+    glutInitWindowSize( 600, 600 );
 
     /* Create an application window on the screen */
     glutCreateWindow( "Assignment #2: 3D Letter" );
@@ -68,10 +76,20 @@ int main( int argc, char *argv[] )
     glutDisplayFunc( Draw );
 
     /* Create the Keyboard callback */
-    glutKeyboardFunc( KeyboardFunc );
+    glutKeyboardFunc( Keyboard );
+
+    /* Create the Reshape callback */
+    glutReshapeFunc( Reshape );
+
+    /* Enable GL Properties */
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
 
     /* Register Exit Handler */
     atexit(ClearMemory);
+
+    /* Initialize the Shape Lists */
+    InitShapes();
 
     /* Turn over control to OpenGL */
     glutMainLoop();
@@ -79,6 +97,225 @@ int main( int argc, char *argv[] )
     return( 0 );  /* NOTE: this is here only for ANSI requirements */
 }
 
+/*-----------------------------------------------------------------------------
+ *  Keyboard
+ *  Handles the keyboard input for each of the various buttons
+ *-----------------------------------------------------------------------------*/
+void Keyboard( unsigned char key, int x, int y )
+{
+    /* Determine which key is pressed */
+    switch(key)
+    {
+        /* Right */
+        case 'd':
+            theta++; break;
+        case 'D':
+            theta += 10; break;
+        /* Left */
+        case 'a':
+            theta--; break;
+        case 'A':
+            theta -= 10; break;
+        /* Up */
+        case 'w':
+            beta++; break;
+        case 'W':
+            beta += 10; break;
+        /* Down */
+        case 's':
+            beta--; break;
+        case 'S':
+            beta -= 10; break;
+        /* Wireframe Modes */
+        case 'F':
+            mode = GL_FILL; break;
+        case 'L':
+            mode = GL_LINE; break;
+        /* Letter Types */
+        case 'Q':
+            if(letter == 't')
+                letter = 'T';
+            else
+                letter = 't';
+            break;
+        default: return; /* Exit if another key was pressed */
+    }
+
+    /* Reshape the Display */
+    Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+
+    /* Redraw the Display */
+    glutPostRedisplay();
+}
+
+/*-----------------------------------------------------------------------------
+ *  Keyboard
+ *  Handles the keyboard input for each of the various buttons
+ *-----------------------------------------------------------------------------*/
+void Reshape( int width, int height )
+{
+    /* Change to Projection Matrix */
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+
+    /* Configure Viewport */
+    glViewport(0, 0, width, height);
+
+    /* Configure Perspective to proper aspect ratio */
+    gluPerspective( 45.0, width / height, 1.0, 500 );
+
+    /* Change to ModelView Matrix */
+    glMatrixMode( GL_MODELVIEW );
+    glLoadIdentity();
+
+    /* Look at Origin from specified angle */
+    gluLookAt((sin(theta * (M_PI / 180)) * radius),
+              0.0, /* Whoopsie */
+              (cos(theta * (M_PI / 180)) * radius),
+              0.0, 0.0, 0.0, /* Look at Center */
+              0.0, 1.0, 0.0); /* Look up */
+}
+
+/*-----------------------------------------------------------------------------
+ *  Keyboard
+ *  Handles the keyboard input for each of the various buttons
+ *-----------------------------------------------------------------------------*/
+void InitShapes ( )
+{
+    /* Create a List */
+    tLists = glGenLists(NUM_LISTS);
+
+    /* Define the List */
+    glNewList( 't', GL_COMPILE );
+
+        /* Draw the Front Face */
+        glColor3f(1.0, 0.0, 0.0);
+        glBegin( GL_POLYGON );
+            glVertex3f( -10.0, 30.0, 12.5 );
+            glVertex3f( -10.0,-50.0, 12.5 );
+            glVertex3f(  10.0,-50.0, 12.5 );
+            glVertex3f(  10.0, 30.0, 12.5 );
+            glVertex3f(  37.5, 30.0, 12.5 );
+            glVertex3f(  37.5, 50.0, 12.5 );
+            glVertex3f( -37.5, 50.0, 12.5 );
+            glVertex3f( -37.5, 50.0, 12.5 );
+            glVertex3f( -37.5, 30.0, 12.5 );
+        glEnd( );
+
+        /* Draw the Back Face */
+        glColor3f(0.0, 1.0, 0.0);
+        glBegin( GL_POLYGON );
+            glVertex3f(  10.0, 30.0,-12.5 );
+            glVertex3f(  10.0,-50.0,-12.5 );
+            glVertex3f( -10.0,-50.0,-12.5 );
+            glVertex3f( -10.0, 30.0,-12.5 );
+            glVertex3f( -37.5, 30.0,-12.5 );
+            glVertex3f( -37.5, 50.0,-12.5 );
+            glVertex3f(  37.5, 50.0,-12.5 );
+            glVertex3f(  37.5, 30.0,-12.5 );
+        glEnd( );
+
+        /* Draw the Inner Sides */
+        glColor3f(0.0, 0.0, 1.0);
+        glBegin( GL_QUAD_STRIP );
+            glVertex3f(  37.5, 50.0,-12.5 );
+            glVertex3f(  37.5, 50.0, 12.5 );
+            glVertex3f(  37.5, 30.0,-12.5 );
+            glVertex3f(  37.5, 30.0, 12.5 );
+            glVertex3f(  10.0, 30.0,-12.5 );
+            glVertex3f(  10.0, 30.0, 12.5 );
+            glVertex3f(  10.0,-50.0,-12.5 );
+            glVertex3f(  10.0,-50.0, 12.5 );
+            glVertex3f( -10.0,-50.0,-12.5 );
+            glVertex3f( -10.0,-50.0, 12.5 );
+            glVertex3f( -10.0, 30.0,-12.5 );
+            glVertex3f( -10.0, 30.0, 12.5 );
+            glVertex3f( -37.5, 30.0,-12.5 );
+            glVertex3f( -37.5, 30.0, 12.5 );
+            glVertex3f( -37.5, 50.0,-12.5 );
+            glVertex3f( -37.5, 50.0, 12.5 );
+            glVertex3f(  37.5, 50.0,-12.5 );
+            glVertex3f(  37.5, 50.0, 12.5 );
+        glEnd( );
+
+    /* End the List */
+    glEndList( );
+
+    /* Define the List */
+    glNewList( 'T', GL_COMPILE );
+        /* Draw the front faces */
+        glColor3f(1.0, 0.0, 0.0);
+        glBegin( GL_POLYGON );
+            glVertex3f( -10.0, 30.0, 8.0 );
+            glVertex3f( -10.0,-50.0, 8.0 );
+            glVertex3f(  10.0,-50.0, 8.0 );
+            glVertex3f(  10.0, 30.0, 8.0 );
+        glEnd( );
+        glBegin( GL_POLYGON );
+            glVertex3f( -37.5, 50.0, 12.5 );
+            glVertex3f( -37.5, 30.0, 12.5 );
+            glVertex3f(  37.5, 30.0, 12.5 );
+            glVertex3f(  37.5, 50.0, 12.5 );
+        glEnd( );
+
+        /* Draw the back faces */
+        glColor3f(0.0, 1.0, 0.0);
+        glBegin( GL_POLYGON );
+            glVertex3f(  10.0, 30.0,-8.0 );
+            glVertex3f(  10.0,-50.0,-8.0 );
+            glVertex3f( -10.0,-50.0,-8.0 );
+            glVertex3f( -10.0, 30.0,-8.0 );
+        glEnd( );
+        glBegin( GL_POLYGON );
+            glVertex3f(  37.5, 50.0,-12.5 );
+            glVertex3f(  37.5, 30.0,-12.5 );
+            glVertex3f( -37.5, 30.0,-12.5 );
+            glVertex3f( -37.5, 50.0,-12.5 );
+        glEnd( );
+
+        /* Draw the Top of the T */
+        glColor3f(0.0, 0.0, 1.0);
+        glBegin( GL_QUAD_STRIP );
+            glVertex3f( -37.5, 30.0,-12.5 );
+            glVertex3f( -37.5, 30.0, 12.5 );
+            glVertex3f( -37.5, 50.0,-12.5 );
+            glVertex3f( -37.5, 50.0, 12.5 );
+            glVertex3f(  37.5, 50.0,-12.5 );
+            glVertex3f(  37.5, 50.0, 12.5 );
+            glVertex3f(  37.5, 30.0,-12.5 );
+            glVertex3f(  37.5, 30.0, 12.5 );
+        glEnd( );
+
+        /* Draw the Bottom of the T */
+        glColor3f(0.0, 0.0, 1.0);
+        glBegin( GL_QUAD_STRIP );
+            glVertex3f(  10.0, 30.0,-8.0 );
+            glVertex3f(  10.0, 30.0, 8.0 );
+            glVertex3f(  10.0,-50.0,-8.0 );
+            glVertex3f(  10.0,-50.0, 8.0 );
+            glVertex3f( -10.0,-50.0,-8.0 );
+            glVertex3f( -10.0,-50.0, 8.0 );
+            glVertex3f( -10.0, 30.0,-8.0 );
+            glVertex3f( -10.0, 30.0, 8.0 );
+        glEnd( );
+
+        /* Draw the Underside of the T */
+        glColor3f(0.0, 0.0, 1.0);
+        glBegin( GL_QUAD_STRIP );
+            glVertex3f( -37.5, 30.0,-12.5 );
+            glVertex3f( -10.0, 30.0,-8.0 );
+            glVertex3f( -37.5, 30.0, 12.5 );
+            glVertex3f( -10.0, 30.0, 8.0 );
+            glVertex3f(  37.5, 30.0, 12.5 );
+            glVertex3f(  10.0, 30.0, 8.0 );
+            glVertex3f(  37.5, 30.0,-12.5 );
+            glVertex3f(  10.0, 30.0,-8.0 );
+            glVertex3f( -37.5, 30.0,-12.5 );
+            glVertex3f( -10.0, 30.0,-8.0 );
+        glEnd( );
+    /* End the List */
+    glEndList( );
+}
 
 /*-----------------------------------------------------------------------------
  *  ClearMemory
@@ -87,6 +324,7 @@ int main( int argc, char *argv[] )
 void ClearMemory( void )
 {
     /* Clear dynamically allocated memory */
+    glDeleteLists( tLists, NUM_LISTS );
 }
 
 
@@ -97,113 +335,17 @@ void ClearMemory( void )
  *-----------------------------------------------------------------------------*/
 void Draw( void )
 {
-    char buf[BUF_SIZ];
-    int i;
-    int starCount = 0;
-
     /* Clear the screen ... */
-    glClear( GL_COLOR_BUFFER_BIT );
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-    /* Resize the viewport */
-    glViewport(0, 0, glutGet(GLUT_WINDOW_WIDTH) * 0.7,
-                     glutGet(GLUT_WINDOW_HEIGHT));
+    /* Set the Polygon Mode */
+    glPolygonMode(GL_FRONT, mode);
 
-    /* Draw the Front Face */
-    glBegin( GL_POLYGON );
-        glVertex3f( -37.5, 50.0, 12.5 );
-        glVertex3f( -37.5, 30.0, 12.5 );
-        glVertex3f( -10.0, 30.0, 12.5 );
-        glVertex3f( -10.0,-50.0, 12.5 );
-        glVertex3f(  10.0,-50.0, 12.5 );
-        glVertex3f(  10.0, 30.0, 12.5 );
-        glVertex3f(  37.5, 30.0, 12.5 );
-        glVertex3f(  37.5, 50.0, 12.5 );
-    glEnd( );
-
-    /* Draw the Back Face */
-    glBegin( GL_POLYGON );
-        glVertex3f(  37.5, 50.0,-12.5 );
-        glVertex3f(  37.5, 30.0,-12.5 );
-        glVertex3f(  10.0, 30.0,-12.5 );
-        glVertex3f(  10.0,-50.0,-12.5 );
-        glVertex3f( -10.0,-50.0,-12.5 );
-        glVertex3f( -10.0, 30.0,-12.5 );
-        glVertex3f( -37.5, 30.0,-12.5 );
-        glVertex3f( -37.5, 50.0,-12.5 );
-    glEnd( );
-
-    /* Draw the Inner Sides */
-    glBegin( GL_QUAD_STRIP );
-        glVertex3f(  37.5, 50.0,-12.5 );
-        glVertex3f(  37.5, 50.0, 12.5 );
-        glVertex3f(  37.5, 30.0,-12.5 );
-        glVertex3f(  37.5, 30.0, 12.5 );
-        glVertex3f(  10.0, 30.0,-12.5 );
-        glVertex3f(  10.0, 30.0, 12.5 );
-        glVertex3f(  10.0,-50.0,-12.5 );
-        glVertex3f(  10.0,-50.0, 12.5 );
-        glVertex3f( -10.0,-50.0,-12.5 );
-        glVertex3f( -10.0,-50.0, 12.5 );
-        glVertex3f( -10.0, 30.0,-12.5 );
-        glVertex3f( -10.0, 30.0, 12.5 );
-        glVertex3f( -37.5, 30.0,-12.5 );
-        glVertex3f( -37.5, 30.0, 12.5 );
-        glVertex3f( -37.5, 50.0,-12.5 );
-        glVertex3f( -37.5, 50.0, 12.5 );
-    glEnd( );
+    /* Call for the List */
+    glCallList(letter);
 
     /* Flush the buffer */
     glutSwapBuffers( ); 
 
     return;
-}
-
-/*-----------------------------------------------------------------------------
- *  KeyboardFunc
- *  Handles the keyboard input for each of the various buttons
- *-----------------------------------------------------------------------------*/
-void KeyboardFunc( unsigned char key, int x, int y )
-{
-    /* Determine which key is pressed */
-    switch(key)
-    {
-        default: return; /* Exit if another key was pressed */
-    }
-
-    /* Redraw the Display */
-    glutPostRedisplay();
-}
-
-
-/*-----------------------------------------------------------------------------
- *  DrawLine
- *  Draws a line at two given points
- *-----------------------------------------------------------------------------*/
-void DrawLine( float x0, float y0, 
-               float x1, float y1 )
-{
-    /* Draw a line  */
-    glBegin( GL_LINES );
-        glVertex2f( x0, y0 );
-        glVertex2f( x1, y1 );
-    glEnd();
-
-    return;
-}
-
-/*-----------------------------------------------------------------------------
- *  DrawText
- *  Draws a set of text using the GLUT bitmap String for a location at x and
- *  y for a set font and buf
- *-----------------------------------------------------------------------------*/
-void DrawText( float x, float y, void* font, char* buf) 
-{
-    /* Set the color */
-    glColor3f(0.0, 0.7, 0.0);
-
-    /* Set the position */
-    glRasterPos2f(x, y);
-
-    /* Print the text */
-    glutBitmapString(font, buf);
 }
