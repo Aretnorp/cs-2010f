a647d4b26b9848af79b42c5f5d842c805e4b8e5b
Cody Thompson <kir.serenity+github@gmail.com>
2010-10-28 15:23:37 -0400

    Added the command line parmemters for the sending and the sending

--- Networking/Lab3/php/server.php
+++ Networking/Lab3/php/server.php
@@ -3,8 +3,14 @@
 # Create the Socket
 $sock = socket_create(AF_INET, SOCK_STREAM, 0);
 
+# Create a directory
+if(!is_dir($argv[2]))
+{
+    mkdir($argv[2]);
+}
+
 # Bind to the socket
-socket_bind($sock, '127.0.0.1', 1026) or die ('Could not bind to address');
+socket_bind($sock, "127.0.0.1", intval($argv[1])) or die ("Could not bind to address\n");
 
 # Listen for new connections
 printf("Listening for connections . . .\n");
@@ -13,8 +19,11 @@ socket_listen($sock);
 # Accept a new connection
 $spawn = socket_accept($sock);
 
+# Receive a file name
+socket_recv($spawn, $file, 128, 0);
+
 # Create the file
-$file = fopen('testfile', 'w+');
+$file = fopen($argv[2] . "/" . $file, 'w+');
 
 # Receive on the connection
 while(($input = socket_recv($spawn, $buf, 1024, 0)) != 0)
