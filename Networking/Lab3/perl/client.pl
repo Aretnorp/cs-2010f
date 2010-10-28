3427a55791ee6c6ae0ae0c3fd6575b609716e4bc
Cody Thompson <kir.serenity+github@gmail.com>
2010-10-28 14:27:18 -0400

    Added command line parameters + sending file names

--- Networking/Lab3/perl/client.pl
+++ Networking/Lab3/perl/client.pl
@@ -3,14 +3,18 @@ use IO::Socket;
 # Open the Socket
 my $sock = new IO::Socket::INET 
 (
-    PeerAddr => '127.0.0.1',
-    PeerPort => '1024',
+    PeerAddr => $ARGV[0],
+    PeerPort => $ARGV[1],
     Proto    => 'tcp',
 );
 die "Could not create socket: $!\n" unless $sock;
 
+# Send the file name
+print "Client has: $ARGV[2] \n";
+print $sock $ARGV[2];
+
 # Open the file
-open FILE, "</usr/bin/vim" or die $!;
+open FILE, "<$ARGV[2]" or die $!;
 
 # Send the information
 print $sock <FILE>;
