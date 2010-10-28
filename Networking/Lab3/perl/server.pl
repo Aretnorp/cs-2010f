1a1aca9ad55b62b8df7f3f2bd1b84a33e52e360c
Cody Thompson <kir.serenity+github@gmail.com>
2010-10-28 14:27:09 -0400

    Added command line parameters + receiving file names

--- Networking/Lab3/perl/server.pl
+++ Networking/Lab3/perl/server.pl
@@ -4,18 +4,30 @@ use IO::Socket;
 my $sock = new IO::Socket::INET 
 (
     LocalAddr => '127.0.0.1',
-    LocalPort => '1024',
+    LocalPort => $ARGV[0],
     Proto    => 'tcp',
     Listen   => 1,
     Reuse    => 1,
 );
 die "Could not create socket: $!\n" unless $sock;
 
-# Open the file for writing
-open FILE, ">testing" or die $!;
-
 # Receive the information
 my $client_sock = $sock->accept();
+
+# Receive the file
+$client_sock->recv($name, 128);
+
+# Create the directory
+unless(-d $ARGV[1])
+{
+    mkdir $ARGV[1] or die $!;
+}
+
+# Open the file for writing
+print STDOUT "Server has >" . "$name\n";
+open FILE, ">", "$ARGV[1]/$name" or die $!;
+
+# Receive the contents of the file
 while(<$client_sock>)
 {
     print FILE $_;
